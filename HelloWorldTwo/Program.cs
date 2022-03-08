67243112311dda5320b203a4959608256e7c1b0d
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-08 18:55:44 +0530

    New Student Added

--- HelloWorldTwo/Program.cs
+++ HelloWorldTwo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!");
+﻿// CS1002.cs  
+namespace x
+{
+    abstract public class clx
+    {
+        int i   // CS1002, missing semicolon  
+
+
+      public static int Main()
+        {
+            return 0;
+        }
+    }
+}
